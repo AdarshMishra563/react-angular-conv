d9ba4ca718cb419c5d927e478283a1f4f9cc9502
Adarsh Mishra <am0070563@gmail.com>
2025-03-23 02:04:04 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -74,7 +74,7 @@ export default function App() {
   return (
      <BrowserRouter>
       <Routes>
-      <Route path="/" element={<Navigate to="/signup" />} />
+      <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp/>} />
         <Route
